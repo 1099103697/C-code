c37963d2dfcb219dd708d1fc7a9f36a47f0d6117
1099103697 <1099103697@qq.com>
2022-07-07 16:28:15 +0800

    提交

---
+++ test_7_7/test_7_7/test_7_7.c
@@ -0,0 +1,90 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+#include<string.h>
+//int main()
+//{
+//	char arr1[] = "bit";
+//	char arr2[20] = "############";
+//	strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[] = "hello world";
+//	memset(arr, '*', 5);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//int get_max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max = get_max(a, b);
+//	printf("%max=%d\n", max);
+//	return 0;
+//}
+
+//void Swap(int* pa, int* pb)
+//{
+//	int tmp = 0;
+//	tmp = *pa;
+//	*pa = *pb;
+//	*pb = tmp;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("a=%d b=%d\n", a, b);
+//	Swap(&a, &b);
+//	printf("a=%d b=%d\n", a, b);
+//	return 0;
+//}
+
+//int is_prime(int n)
+//{
+//	int j = 0;
+//	for (j = 2; j < n; j++)
+//	{
+//		if (n%j == 0)
+//			return 0;
+//	}
+//	return 1;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	for(i = 100; i <= 200; i++)
+//	{
+//		if (is_prime(i) == 1)
+//			printf(" %d", i);
+//	}
+//	return 0;
+//}
