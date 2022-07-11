3dae5ff8f34007c058313f6773219e1ea5e161be
1099103697 <1099103697@qq.com>
2022-07-11 17:34:40 +0800

    1

---
+++ test_7_11/test_7_11/test_7_11.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	//创建一个数组-存放整形——10个
+//	int arr[10]={1,2,3};//不完全初始化，剩下的元素默认初始化为0
+//	char arr2[5];
+//	int n = 5;
+//	//char ch[n];//[]只能是常量
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a', 'b', 'c' };
+//	printf("%d\n", sizeof(arr1));//4
+//	printf("%d\n", sizeof(arr2));//3
+//	printf("%d\n", strlen(arr1));//3
+//	printf("%d\n", strlen(arr2));//随机值
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	//printf("%c\n", arr[3]);
+//	int i = 0;
+//	int len = strlen(arr);
+//	for (i = 0; i <= 6; i++)
+//	{
+//		printf("%c\n", arr[i]);
+//	}
+//
+//	for (i = 0; i <len; i++)
+//	{
+//		printf("%c\n", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+}
